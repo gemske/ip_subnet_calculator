876f661c8ba47d63c9c4178367bc32da03600e44
Chris <chris.gemske@gmail.com>
2017-07-09 19:14:40 -0400

    commented out alternate logic for address type function

--- subnet_calc.rb
+++ subnet_calc.rb
@@ -1,3 +1,5 @@
+require 'rubygems'  # optional
+require 'ipaddress'
 # this is defining the function to convert to binary from ip array
 def to_binary(address_input)
   address_arr = address_input.split('.')
@@ -15,18 +17,18 @@ def binary_host_portion(ip_sub_map)
 end
 
 # returns type of ip/sub address
-# options network broadcast host
 def address_type(host_portion)
   if host_portion.include?('0') && !host_portion.include?('1')
     ipclass = 'network'
-  elsif host_portion.chars.reject{ |x| x == '1' }.empty?
+  elsif host_portion.include?('1') && !host_portion.include?('0')
+ #elsif host_portion.chars.reject{ |x| x == '1' }.empty?
     ipclass = 'broadcast'
   else
     ipclass = 'host'
   end
 end
 
-def network_value(subnet_binary)
+def network_bits(subnet_binary)
   subnet_binary.to_s.split("").count("1").to_i
 end
 
@@ -34,12 +36,12 @@ def host_value(subnet_binary)
    subnet_binary.to_s.split("").count("0").to_i
 end
 
-def net_valid?(subnet_binary, network_value)
-  subnet_binary.chars.slice(0, network_value).include?('0')
+def net_valid?(subnet_binary, network_bits)
+  subnet_binary.chars.slice(0, network_bits).include?('0')
 end
 
-def available_networks(network_value)
-  available_net = Array.new(network_value, 1)
+def available_networks(network_bits)
+  available_net = Array.new(network_bits, 1)
   total_networks = 0
   available_net.each_with_index do |n, i|
     total_networks += 2 ** (available_net.length-i-1) * n
@@ -50,6 +52,7 @@ end
 def host_ip_addr(host_address)
 end
 
+#TERMINAL
 puts "=================================================================="
 puts ""
 puts "Chris' IP subnet calculator"
@@ -59,7 +62,7 @@ puts "Enter IP address."
 ip_input = gets
 puts "Enter subnet mask."
 subnet_input = gets
-
+#TERMINAL
 
 ip_binary = to_binary(ip_input)
 subnet_binary = to_binary(subnet_input)
@@ -71,12 +74,13 @@ host_portion = binary_host_portion(ip_sub_map).join
 address_type = address_type(host_portion)
 host_1_count = host_portion.count('1')
 host_0_count = host_portion.count('0')
-network_value = network_value(subnet_binary)
+network_bits = network_bits(subnet_binary)
 host_value = host_value(subnet_binary)
-net_valid = net_valid?(subnet_binary, network_value)
-available_networks = available_networks(network_value)
+net_valid = net_valid?(subnet_binary, network_bits)
+available_networks = available_networks(network_bits)
+v = net_valid ? 'WARNING: INVALID SUBNET' : "Your IP address: #{ip_input.chomp}/#{network_bits}"
 
-v = net_valid ? 'WARNING: INVALID SUBNET' : "Your IP address: #{ip_input.chomp}/#{network_value}"
+#TERMINAL
 puts " "
 puts " "
 puts "================================================================== "
@@ -84,7 +88,7 @@ puts v
 puts "Address Type: #{address_type}"
 puts "================================================================== "
 puts "Available Networks: #{available_networks}"
-puts "Subnet Prefix: /#{network_value} prefix."
+puts "Subnet Prefix: /#{network_bits} prefix."
 puts " "
 puts " "
 puts "======================BINARY CHART================================ "
@@ -92,8 +96,10 @@ puts " "
 puts "IP:     " + ip_binary
 puts "SUBNET: " + subnet_binary
 puts " "
+#TERMINAL
 
-#net_count = network_value.to_binary.ljust(net_count_value, '0')
+
+#net_count = network_bits.to_binary.ljust(net_count_value, '0')
 #host_count = host_value.to_binary.ljust(host_count_value, '0')
 #puts net_count
 #puts host_count
